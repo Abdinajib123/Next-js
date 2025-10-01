acd0a950accbf09756f172913b7b3e0ff35f1bc6
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2025-10-01 17:31:35 +0300

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts= await data.json();
+  console.log(posts);
+  
   return (
    <div>
-    <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    <h1 className="text-3xl font-bold underline">Home Page</h1>
+    {
+      posts.map(post=>(
+        <div key={post.id} className="p-4 m-4 border rounded shadow">
+          <h2 className="text-2xl font-semibold">{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))
+    }
+  
    </div>
   );
 }
