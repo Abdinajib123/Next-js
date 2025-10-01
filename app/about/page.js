cb5c97d109490e6a642ec3e7726c7aee02e5de9c
Abdinajib123 <cabdicabdulaahi8899@gmail.com>
2025-10-01 15:10:47 +0300

    pages

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+const Aboutpage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">About Page</h1>
+        </div>
+    )
+}
+
+export default Aboutpage;
